0bb5ce084356f10311272907262b8d1500e3591f
FacistFuhrer <mikebao1999@gmail.com>
2017-07-29 14:33:10 -0700

    Added edit_distance

--- autocorrect.py
+++ autocorrect.py
@@ -22,12 +22,19 @@ def autocorrect(word):
 
 
 def edit_distance(word1, word2):
-	dp = []
-	for 
+	dp = [[0]*len(word1)]*len(word2)
+	for let2 in range(len(word2)):
+		for let1 in range(len(word1)):
+			if word1[let1]==word2[let2]:
+				dp[let2][let1] = dp[let2-1][let1-1]
+			else:
+				dp[let2][let1] = min(dp[let2-1][let1] + 1, dp[let2][let1-1] + 1, dpl[let2-1][let1-1] + 1)
+	return dp[len(word2)-1][len(word1)-1]
 
 
 if __name__ == '__main__':
-	autocorrect(raw_input('Input a string: '))
+	#autocorrect(raw_input('Input a string: '))
+	edit_distance(raw_input('Input a string: '), raw_input('Input a string: '))
 
 
 
