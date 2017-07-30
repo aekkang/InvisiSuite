6aedf53edb23d7728b3ab51676763d292944cbcb
aekkang <akang@caltech.edu>
2017-07-29 18:17:32 -0700

    Current working heuristics-based version

--- stream.py
+++ stream.py
@@ -1,5 +1,6 @@
 # Adapted from: https://gist.github.com/shihyuan/4d834d429763e953a40c
 
+import math
 import time
 import urllib2
 import sys
@@ -9,7 +10,7 @@ import cv2
 import skimage.measure
 
 
-N_STREAMS = 2
+N_STREAMS = 1
 
 # Start streams.
 streams = []
@@ -25,44 +26,36 @@ for i in range(N_STREAMS):
     print('Streaming {}'.format(hoststr))
 
 
-letter = ''
-word = ''
 init = False
 calibration = 0
-frame_rate = 2
-
-def checker(new):
-    global letter
-    if letter != new:
-        letter = new
-        return new
-
-    return ''
-
+n_frame = 0
+n_last = 0
 
 streamstr_lst = ['', '']
 
 while True:
-    for i in range(2):
+    n_frame += 1
+    for i in range(N_STREAMS):
         streamstr_lst[i] += streams[i].read(1024)
 
-    streamstr = streamstr_lst[1]
+    streamstr = streamstr_lst[0]
     start = 0
-    for i in range(frame_rate):
-        start = streamstr.find('\xff\xd8', start + 1)
-        if start == -1:
-            break
+    # for i in range(frame_rate):
+    start = streamstr.find('\xff\xd8')#, start + 1)
+    # if start == -1:
+    #     break
 
     end = streamstr.find('\xff\xd9', start)
     
     if start != -1 and end != -1:
         img_raw = streamstr[start:end+2]
-        streamstr_lst[i] = streamstr[end+2:]
+        streamstr_lst[0] = streamstr[end+2:]
+
         frame = cv2.imdecode(np.fromstring(img_raw, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
         
-        rthresh = (frame[:,:,0] < 30).astype(np.uint8)
-        gthresh = (frame[:,:,1] < 30).astype(np.uint8)
-        bthresh = (frame[:,:,2] < 30).astype(np.uint8)
+        rthresh = (frame[:,:,0] < 50).astype(np.uint8)
+        gthresh = (frame[:,:,1] < 50).astype(np.uint8)
+        bthresh = (frame[:,:,2] < 50).astype(np.uint8)
         thresh = (rthresh + gthresh + bthresh == 3)
         empty = np.zeros_like(frame[:,:,2])
 
@@ -79,26 +72,39 @@ while True:
         #     letter = 'f'
         #     init = True
 
-        labels, _ = skimage.measure.label(frame[:,:,0], return_num=True)
-        uniques, counts = np.unique(labels[1:], return_counts=True)
-        label_finger = uniques[np.argmax(counts)] + 1
+        labels, _ = skimage.measure.label(thresh, return_num=True)
+        uniques, counts = np.unique(labels, return_counts=True)
+        label_finger = uniques[np.argmax(counts[1:]) + 1]
         x, y = np.where(labels == label_finger)
+
+        # x, y = np.where(frame[:,:,0])
 
         if not init:
             calibration = np.min(x)
+            prev = np.min(y)
+            curr = np.min(y)
             init = True
 
         pos = np.min(x)
+        prev = curr
+        curr = np.min(y)
+        clicked = math.fabs(curr - prev) >= 10
 
-        if pos < calibration * 0.9:
-            word += checker('r')
-        elif pos < calibration * 1.1:
-            word += checker('f')
-        elif pos < calibration * 1.3:
-            word += checker('v')
-        elif pos < calibration * 1.5:
-            print(word)
-            word = ''
+        if pos < calibration * 0.9 and clicked:
+            if n_frame - n_last > 200:
+                print('r')
+                n_last = n_frame
+        elif pos < calibration * 1.1 and clicked:
+            if n_frame - n_last > 200:
+                print('f')
+                n_last = n_frame
+        elif pos < calibration * 1.3 and clicked:
+            if n_frame - n_last > 200:
+                print('v')
+                n_last = n_frame
+        # elif pos < calibration * 1.5:
+        #     print(word)
+        #     word = ''
 
         if cv2.waitKey(1) == 27:
             break
